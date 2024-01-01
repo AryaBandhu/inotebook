c5dc156718ee29251245ee8010b61416fcd5717a
Arya Bandhu <73266677+AryaBandhu@users.noreply.github.com>
2024-01-01 15:50:44 +0530

    Insert Route & Optimize the React App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
 import './App.css';
+import HomePage from './components/HomePage';
+import About from './components/About';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <h1>Arya Bandhu App</h1>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<HomePage/>} />
+          <Route exact path="/about" element={<About/>} />
+        </Routes>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
